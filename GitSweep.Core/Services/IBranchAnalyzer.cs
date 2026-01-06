20034ed1cf438e749b0a0debfc0e9d405489f151
Sergio Triana Escobedo <striana@apexsystems.com>
2026-01-06 15:41:08 -0600

    Add branch analysis and Git repo service interfaces
    Introduced IBranchAnalyzer and IGitRepositoryService interfaces in the GitSweep.Core.Services namespace. IBranchAnalyzer provides methods to identify stale and merged branches using BranchInfo. IGitRepositoryService offers async methods for repository checks, default branch retrieval, and listing local branches. Both interfaces depend on the BranchInfo model.

---
+++ GitSweep.Core/Services/IBranchAnalyzer.cs
@@ -0,0 +1,9 @@
+﻿using GitSweep.Core.Models;
+
+namespace GitSweep.Core.Services;
+
+public interface IBranchAnalyzer
+{
+    IEnumerable<BranchInfo> IdentifyStaleBranches(IReadOnlyList<BranchInfo> branches, int staleAgeInMonths);
+    IEnumerable<BranchInfo> IdentifyMergedBranches(IReadOnlyList<BranchInfo> branches);
+}
---
+++ GitSweep.Core/Services/IGitRepositoryService.cs
@@ -0,0 +1,10 @@
+﻿using GitSweep.Core.Models;
+
+namespace GitSweep.Core.Services;
+
+public interface IGitRepositoryService
+{
+    Task<bool> IsGitRepositoryAsync(string path, CancellationToken ct = default);
+    Task<string> GetDefaultBranchNameAsync(string path, CancellationToken ct = default);
+    Task<List<BranchInfo>> GetLocalBranchesAsync(string path, string targetBranch, CancellationToken ct = default);
+}
